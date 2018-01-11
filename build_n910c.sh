f92392cb68d5805a331a23a8789d5ef1dc1e8a69
ananjaser1211 <ananjaser@gmail.com>
2018-01-11 23:32:56 +0400

    DtbTool Thanks to LucasAddon for DTB script for N910C

--- build_n910c.sh
+++ build_n910c.sh
@@ -4,20 +4,34 @@
 DTS=arch/arm/boot/dts
 RDIR=$(pwd)
 # GCC
-export CROSS_COMPILE=
+export CROSS_COMPILE=/home/elite/android/toolchain/arm-eabi-4.8/bin/arm-eabi-
 # Cleanup
 make clean && make mrproper
 # N910C_NN Config
 make 
-make 
+make exynos5433-tre_eur_open_07.dtb
+make exynos5433-tre_eur_open_08.dtb
+make exynos5433-tre_eur_open_09.dtb
+make exynos5433-tre_eur_open_10.dtb
+make exynos5433-tre_eur_open_12.dtb
+make exynos5433-tre_eur_open_13.dtb
+make exynos5433-tre_eur_open_14.dtb
+make exynos5433-tre_eur_open_16.dtb
 # Make zImage
 make ARCH=arm -j4
-./scripts/dtbTool/dtbTool -o ./boot.img-dtb -d $DTS/ -s 2048
-# Cleaup
+echo -n "Build dt.img......................................."
+
+./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $DTS/
+# get rid of the temps in dts directory
 rm -rf $DTS/.*.tmp
 rm -rf $DTS/.*.cmd
 rm -rf $DTS/*.dtb
-# Generate Boot_N910c.img
+
+# Calculate DTS size for all images and display on terminal output
+du -k "./dt.img" | cut -f1 >sizT
+sizT=$(head -n 1 sizT)
+rm -rf sizT
+echo "$sizT Kb"
 
 echo "Remove Any files"
 cd /home/elite/android/refinedkernel_2/rf-tools/AIK-Linux
--- dts.sh
+++ dts.sh
@@ -2,12 +2,26 @@
 #!/bin/bash
 DTS=arch/arm/boot/dts
 RDIR=$(pwd)
-# UberTC
-export CROSS_COMPILE=
-make 
-# Make DT.img
-./scripts/dtbTool/dtbTool -o ./boot.img-dtb -d $DTS/ -s 2048
-# Cleaup
+# GCC
+export CROSS_COMPILE=/home/elite/android/toolchain/arm-eabi-4.8/bin/arm-eabi-
+make exynos5433-tre_eur_open_07.dtb
+make exynos5433-tre_eur_open_08.dtb
+make exynos5433-tre_eur_open_09.dtb
+make exynos5433-tre_eur_open_10.dtb
+make exynos5433-tre_eur_open_12.dtb
+make exynos5433-tre_eur_open_13.dtb
+make exynos5433-tre_eur_open_14.dtb
+make exynos5433-tre_eur_open_16.dtb
+echo -n "Build dt.img......................................."
+
+./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $DTS/
+# get rid of the temps in dts directory
 rm -rf $DTS/.*.tmp
 rm -rf $DTS/.*.cmd
 rm -rf $DTS/*.dtb
+
+# Calculate DTS size for all images and display on terminal output
+du -k "./dt.img" | cut -f1 >sizT
+sizT=$(head -n 1 sizT)
+rm -rf sizT
+echo "$sizT Kb"
