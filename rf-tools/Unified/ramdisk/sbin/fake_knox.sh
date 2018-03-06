9f4112e646c975d63c763c7e22a680abc1819d54
ananjaser1211 <ananjaser@gmail.com>
2018-03-07 01:21:22 +0400

    resetprop > fakeprop
    resetprop conflicts with magisk one

--- rf-tools/Unified/ramdisk/sbin/fake_knox.sh
+++ rf-tools/Unified/ramdisk/sbin/fake_knox.sh
@@ -16,15 +16,15 @@
 # Thanks to @Blackmesa123 @Tkkg1994
 
 # Knox set to 0 on running system
-/sbin/resetprop -n ro.boot.warranty_bit "0"
-/sbin/resetprop -n ro.warranty_bit "0"
+/sbin/fakeprop -n ro.boot.warranty_bit "0"
+/sbin/fakeprop -n ro.warranty_bit "0"
 # Fix safetynet flags
-# /sbin/resetprop -n ro.boot.veritymode "enforcing"
-# /sbin/resetprop -n ro.boot.verifiedbootstate "green"
-# /sbin/resetprop -n ro.boot.flash.locked "1"
-# /sbin/resetprop -n ro.boot.ddrinfo "00000001"
-/sbin/resetprop -n ro.build.selinux "1"
+# /sbin/fakeprop -n ro.boot.veritymode "enforcing"
+# /sbin/fakeprop -n ro.boot.verifiedbootstate "green"
+# /sbin/fakeprop -n ro.boot.flash.locked "1"
+# /sbin/fakeprop -n ro.boot.ddrinfo "00000001"
+/sbin/fakeprop -n ro.build.selinux "1"
 # Samsung related flags
-/sbin/resetprop -n ro.fmp_config "1"
-# /sbin/resetprop -n ro.boot.fmp_config "1"
-# /sbin/resetprop -n sys.oem_unlock_allowed "0"
+/sbin/fakeprop -n ro.fmp_config "1"
+# /sbin/fakeprop -n ro.boot.fmp_config "1"
+# /sbin/fakeprop -n sys.oem_unlock_allowed "0"
